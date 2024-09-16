c4af67cf5489007d5fbc9da432b549c31a7d1b0a
Klemen Pavlovic <klemenpavlovic@gmail.com>
2024-09-16 11:05:12 +0200

    Add comment links

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,12 @@ const checkIsDarkSchemePreferred = () => {
 // Dynamic seems to be pricier if your app scales => In Dashboard its said that standard pricing plan offers the first 200 MAUs (Monthly Active Users) for free, followed by 99$ for up to 2000 MAUs and then charges additional 0.05$ per MAU thereafter, but on on the pricing page it says that standard plan is first 1000 MAUs free and then again after 2000 MAUs every new user is additional 0.05$
 // Functionality seems ok, also good customization, but would it be worth the extra price?
 // The web dashboard of dynamic is extensive, more functionality compared to wallet connect, particularly when it comes to user management, but as mentioned the price is also greater
+// Dynamic might make more sense if you are building a serious app, where there could be a lot of user's and you need more advanced user management
+// Dynamic works as SIWE (sign in with ethereum) by default, no need for another library like next-auth, which we need with appkit?
+
+// Links
+// https://demo.dynamic.xyz/ => Demo of wallet usage
+// https://docs.dynamic.xyz/introduction/welcome => Docs
 
 export default function Main() {
   const [isDarkMode, setIsDarkMode] = useState(checkIsDarkSchemePreferred);
